19ddcb001768b7410060898866de5f9363c3792f
Ксения <123957308+KseniaAnt@users.noreply.github.com>
2024-07-16 10:15:08 +0300

    Add files via upload
    #feat

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
+            System.out.print("\n");
             System.out.print(
-                    "\nВведите выражение (Первое число, операция(+, -, *, /), второе число. Между числами и знаком пробел): ");
+                    "\n Введите выражение (Первое число, операция(+, -, *, /), второе число. Между числами и знаком пробел): ");
             String expression = in.nextLine();
             Calculator.calc(expression);
         }
